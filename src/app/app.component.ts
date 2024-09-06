ce3bc7176afec17f032b9fe270f677924aa27e7e
Chil's Coding World <chilhoank@yahoo.ca>
2024-09-06 15:34:18 -0400

    Angular signal examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Product } from '../product';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-signals-example';
+  allProducts: Product[] = [
+    { id: 1, name: 'milk', price: 1.45 },
+    { id: 2, name: 'break', price: 3.45 },
+    { id: 3, name: 'apple', price: 2.45 },
+  ];
 }
